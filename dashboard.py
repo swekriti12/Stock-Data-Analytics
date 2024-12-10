79207bd5c4b34f3f66b6e581b7e2afc0b31c5056
= <=>
2024-12-10 17:26:50 -0500

    Renamed .txt file

--- dashboard.py
+++ dashboard.py
@@ -25,7 +25,7 @@ def get_sp500_tickers():
 tickers_list = get_sp500_tickers()
 
 #Set title for the dashboard
-st.title('Stock Market Analysis Dashboard')
+st.title('Stock Market Dashboard')
 
 #Create a dropdown menu with a search bar to allow users to select a ticker or enter a custom ticker
 ticker = st.sidebar.selectbox('Select Ticker', tickers_list + ['Other'], key="ticker_dropdown", index=tickers_list.index('GOOGL') if 'GOOGL' in tickers_list else 0)
